03b6f3e34b6adafa998d1ecc41c04b3763d03dba
vancouver29 <vancouver2990@gmail.com>
2022-12-02 12:40:48 +0100

    create task tracker vuejs

---
+++ src/components/Button.vue
@@ -0,0 +1,22 @@
+<template>
+    <button @click="onClick()" :style="{background: color}" class="btn">{{text}}</button>
+</template>
+
+<script>
+    export default {
+        name: "Button",
+        props: {
+            text: {
+                type: String
+            },
+            color: {
+                type: String
+            }
+        },
+        methods: {
+            onClick() {
+                this.$emit("btn-click")
+            }
+        }
+    }
+</script>
